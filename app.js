5fa826ea83de1457396115fb22d9d88d11c3e877
Mia Jarnot <ejarnot42@outlook.com>
2022-03-28 10:00:43 -0700

    Finished exercise in app.js

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arraySum(numbers){
+    let sum = 0
+    function add(num){
+        sum += num;
+    }
+    numbers.forEach(add);
+    
+    /*for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }*/  
+    
+    return sum;
+};
+console.log(arraySum(numbers));
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+let book = {
+    title: "The Amber Spyglass",
+    author: "Phillip Pullman",
+    pages: 518,
+    readCount: 5,
+}
+book.info = `${book.title} by ${book.author}, ${book.pages} pages, read ${book.readCount} times.`;
+console.log(book.info);
 
 
